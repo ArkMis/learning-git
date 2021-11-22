b8aa23f40a0a130db1715d86301719bfcd6cd099
Arkadiusz Misiewicz <misiewicz.arkadiusz@gmail.com>
2021-11-23 00:45:08 +0100

    usunięcie zadania 2

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -1,7 +1,7 @@
 # Zadanie 1
 shop_dict=dict()
-shop_dict['piekarnia']=['chleb', 'pączek', 'bułki']
-shop_dict['warzywniak']=['marchew', 'seler', 'rukola']
+shop_dict['cukiernia']=['chałka', 'pączek', 'bułki']
+shop_dict['warzywniak']=['marchew', 'seler', 'pietruszka','ziemniaki']
 print('Lista zakupów')
 count=0
 for sklep in shop_dict:
@@ -9,14 +9,4 @@ for sklep in shop_dict:
        count+=len(shop_dict[sklep])
     print('Idę do '+sklep.capitalize()+', kupuję następujące rzeczy:'+repr(shop_dict[sklep]).title())
 print('W sumie kupuję '+repr(count)+' produktów')
-
-# Zadanie 2
-numbers=[]
-numbers3=[]
-for x in range(0,101):
-    if x%5==0:
-        numbers.append(x)
-print(numbers)
-numbers3=[x**3 for x in numbers]
-print(numbers3)
 
