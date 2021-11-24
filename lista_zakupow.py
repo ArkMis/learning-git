5397f74eb7b531cea6f02eb12e64d23ddc81364c
ArkMis <94876297+ArkMis@users.noreply.github.com>
2021-11-24 17:21:03 +0100

    Update lista_zakupow.py

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -1,8 +1,8 @@
 # Zadanie 1
 # specjalnie dla Jana
 shop_dict=dict()
-shop_dict['cukiernia']=['chałka', 'pączek', 'bułki']
 shop_dict['warzywniak']=['marchew', 'seler', 'pietruszka','ziemniaki']
+shop_dict['cukiernia']=['chałka', 'pączek', 'bułki']
 print('Lista zakupów')
 count=0
 for sklep in shop_dict:
